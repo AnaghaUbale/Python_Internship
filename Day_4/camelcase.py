1ce7f84acd21da288093d7390b7da547f7f396f7
AnaghaUbale <anaghaubale11@gmail.com>
2022-08-02 20:37:21 +0530

    Variables

---
+++ Day_4/camelcase.py
@@ -0,0 +1,42 @@
+import camelcase
+
+c=camelcase.CamelCase()
+
+txt="shiv is handsome boy,he is very smart"
+
+print(c.hump(txt))
+
+#This method capitalizes the first letter of each word.
+
+#Variables
+
+x = 5
+y = "Shiv"
+print(x)
+print(y)
+
+x = "Anagha"
+print(x)
+
+#Typecasting
+
+x = str(1000000) #x will be '1000000'
+y = int(500) #y will be '500'
+z = float(00000000) 
+
+print(x)
+print(y)
+print(z)
+
+#Checking the datatypes of variable
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#Checking the case sensitivity
+
+X = "This is a capital X"
+
+print("Small x = ",x)
+print("Capital X = ",X)
